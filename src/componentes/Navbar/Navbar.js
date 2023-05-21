30c72f16cf8552092fe2e60431a89f73c30e325a
허건녕 <heo3793@naver.com>
2023-05-21 18:27:15 +0900

    페이지라우팅

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const StyledNavbar = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: red;
+`;
+
+export default function Navbar() {
+  return <StyledNavbar></StyledNavbar>;
+}
