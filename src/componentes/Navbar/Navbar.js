360f23bcb792d770532ba255a15de8a235d68549
허건녕 <heo3793@naver.com>
2023-05-21 22:04:50 +0900

    Navbar임시 버튼제작

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { styled } from "styled-components";
+import { NavbarButton } from "../../items/Navbar/NavbarButton";
+import { useLocation } from "react-router-dom";
 
 const StyledNavbar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100px;
-  background-color: red;
+  background-color: #487eb0;
+
+  // flex는 글로벌 스타일로 설정
+  .flex {
+    max-width: 1024px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `;
 
 export default function Navbar() {
-  return <StyledNavbar></StyledNavbar>;
+  const { pathname } = useLocation();
+  return (
+    <StyledNavbar>
+      <div className="flex">
+        <div className="flex-left">
+          <NavbarButton size="medium" label="로고" />
+          <NavbarButton
+            $primary={pathname === "/"}
+            size="medium"
+            label="Sara"
+          />
+          <NavbarButton size="medium" label="페이지" />
+        </div>
+        <div className="flex-right">
+          <NavbarButton size="small" label="로그인" />
+          <NavbarButton size="small" label="설정" />
+        </div>
+      </div>
+    </StyledNavbar>
+  );
 }
---
+++ src/items/Navbar/NavbarButton.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { css, styled } from "styled-components";
+import PropTypes from "prop-types";
+
+const getVariantStyles = ({ $primary = false }) =>
+  $primary
+    ? css`
+        color: red;
+        font-size: 24px;
+      `
+    : css`
+        color: black;
+        font-size: 20px;
+      `;
+
+const getSizeStyles = ({ size = "medium" }) => {
+  switch (size) {
+    case "small": {
+      return css`
+        font-size: 12px;
+      `;
+    }
+
+    default: {
+      return css`
+        font-size: 20px;
+      `;
+    }
+  }
+};
+
+const StyledNavbarButton = styled.button`
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  padding: 15px;
+  margin: 5px;
+  cursor: pointer;
+  display: inline-block;
+  background-color: transparent;
+
+  ${(props) => getVariantStyles(props)}
+  ${(props) => getSizeStyles(props)}
+`;
+
+export const NavbarButton = ({ label, ...rest }) => {
+  console.log(rest);
+  return <StyledNavbarButton {...rest}>{label}</StyledNavbarButton>;
+};
+
+NavbarButton.defaultProps = {
+  $primary: false,
+  size: "medium",
+  onClick: undefined,
+};
