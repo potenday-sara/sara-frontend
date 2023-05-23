523e5bbba2fecbfc8b85c13a53190ec0d97b4137
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2023-05-23 23:47:49 +0900

    release v0.01 (#2)
    * develop
    
    * Feature/페이지레이아웃제작 (#1)
    
    * 페이지라우팅
    
    * Navbar임시 버튼제작
    
    * Navbar 스토리북 제작
    
    * 스토리북배포
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트
    
    * Github Action 테스트

---
+++ src/items/Navbar/NavbarButton.stories.js
@@ -0,0 +1,54 @@
+import { NavbarButton } from "./NavbarButton";
+
+export default {
+  title: "NavbarButton",
+  tags: ["autodocs"],
+  component: NavbarButton,
+};
+
+export const NowPage = {
+  args: {
+    $primary: true,
+    size: "medium",
+    label: "Sara",
+  },
+};
+
+export const NotNow = {
+  args: {
+    $primary: false,
+    size: "medium",
+    label: "페이지",
+  },
+};
+
+export const Login = {
+  args: {
+    $primary: false,
+    size: "small",
+    label: "로그인",
+  },
+};
+
+export const Test = {
+  args: {
+    $primary: false,
+    size: "small",
+    label: "로그인",
+  },
+};
+
+export const Test2 = {
+  args: {
+    $primary: false,
+    size: "small",
+    label: "로그인",
+  },
+};
+export const Test3 = {
+  args: {
+    $primary: false,
+    size: "small",
+    label: "로그인",
+  },
+};
