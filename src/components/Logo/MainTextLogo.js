9da226b25ca9f9dc411fe99577888541fa50274a
heogeon0 <heo3793@naver.com>
2023-09-02 22:18:38 +0900

    conflict resolve

--- src/components/Logo/MainTextLogo.js
+++ src/components/Logo/MainTextLogo.js
@@ -1,11 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-<<<<<<< HEAD
 import { ReactComponent as Sara } from "../../images/logos/saratextlogo.svg";
-=======
-import { ReactComponent as Sara } from "../../images/logos/miainlogo.svg";
->>>>>>> 90f85befeb75d7d7215ac98e2fbd412fb48e136d
 import { ReactComponent as Mara } from "../../images/logos/maratextlogo.svg";
 import { styled } from "styled-components";
 
@@ -21,10 +17,7 @@ const StyledMainTextLogo = styled.div`
 export default function MainTextLogo({ $type, ...rest }) {
   // return <StyledMainTextLogo className="textLogo">{$type === "sara" ? <Sara /> : <Mara />}</StyledMainTextLogo>;
   return <StyledMainTextLogo {...rest} className="textLogo">{$type === 'sara' ? <Sara /> : <Mara />}</StyledMainTextLogo>;
-<<<<<<< HEAD
   // return <StyledMainTextLogo {...rest} className="textLogo"><Sara /></StyledMainTextLogo>;
-=======
->>>>>>> 90f85befeb75d7d7215ac98e2fbd412fb48e136d
 }
 
 MainTextLogo.propTypes = {
