a6a9fd2801e8ae3f9fc8f20f4efd704d58c51c04
heogeon0 <heo3793@naver.com>
2023-09-02 20:37:22 +0900

    로고 비침 현상 해결

--- src/features/onboarding/styles/getSelectedStyle.js
+++ src/features/onboarding/styles/getSelectedStyle.js
@@ -9,6 +9,10 @@ export const getSelectedCss = (isSeleted) => {
   if (isSeleted) return css`
     opacity: 1.0;
     scale: 1.2;
+    .back {
+      z-index: -1;
+    }
+
     .back-sara {
       left: -43%;
       width: 380px;
