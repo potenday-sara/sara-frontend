3a863f6c02c4fe966660e51889fc54441fc7d690
heogeon0 <heo3793@naver.com>
2023-11-19 22:44:35 +0900

    로고컴포넌트 수정

--- src/features/main/components/organisms/RealtimeTrend/index.jsx
+++ src/features/main/components/organisms/RealtimeTrend/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import StyledRealtimeTrend from './styles';
 import TrendLabels from '../../molecules/TrendLabels';
-import TrendTitle from '../../molecules/TrendTitle';
+import TrendTitle from '../../atoms/TrendTitle';
 import SaraMaraContext from '../../../stores/SaraMaraContext';
 
 export default function RealtimeTrend() {
