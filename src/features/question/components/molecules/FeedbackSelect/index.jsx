5895527c32c3b3901e71e1c0f1162c0175a16880
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2023-12-13 23:10:36 +0900

    SAR-62-Select코드수정 (#103)

--- src/features/question/components/molecules/FeedbackSelect/index.jsx
+++ src/features/question/components/molecules/FeedbackSelect/index.jsx
@@ -9,35 +9,50 @@ import useEmotionFeedback from '../../../hooks/useEmotionFeedback';
 import Toast from '../../../../../components/molecules/Toast';
 import Text from '../../../../../components/atoms/Text';
 import { Theme } from '../../../../../Styles';
+import Select from '../../../../../components/molecules/Select';
+import { css } from 'styled-components';
+
+const feedbackOptions = [
+  [<Good />, '도움이 됐어요', 1],
+  [<Soso />, '그럭저럭...?!', 0],
+  [<Bad />, '별로에요 우우~', -1],
+];
+
+const feedbackStyle = css`
+  position: relative;
+  width: 100%;
+  display: flex;
+  gap: 24px;
+  justify-content: center;
+`;
 
 export default function FeedbackSelect({ type, quesionId }) {
-  const { nowSelected, getNowSelectedFeedback, isToast } = useEmotionFeedback(quesionId);
-  const feedbackOptions = [
-    [<Good />, '도움이 됐어요', 1],
-    [<Soso />, '그럭저럭...?!', 0],
-    [<Bad />, '별로에요 우우~', -1],
-  ];
+  const { nowSelected, setNowSelectedFeedback, isToast } = useEmotionFeedback(quesionId);
 
   return (
     <StyledFeedbackSelect>
-      {feedbackOptions.map(([option, label, score], idx) => {
-        return (
-          <FeedbackEmotion
-            type={type}
-            emotion={option}
-            key={['Emotion', idx]}
-            isActivated={nowSelected === score}
-            onClick={() => getNowSelectedFeedback(score)}
-          >
-            <FeedbackEmotion.Label label={label} />
-            {isToast && nowSelected === score && (
-              <Toast style={{ width: 130 }}>
-                <Text label="평가가 반영되었어요!" color={Theme.color.white} size="12px" bold="400" />
-              </Toast>
-            )}
-          </FeedbackEmotion>
-        );
-      })}
+      <Select value={nowSelected}>
+        <Select.List cssstyle={feedbackStyle}>
+          {feedbackOptions.map(([option, label, score], idx) => {
+            return (
+              <FeedbackEmotion
+                type={type}
+                emotion={option}
+                key={['Emotion', idx]}
+                isActivated={nowSelected === score}
+                onClick={() => setNowSelectedFeedback(score)}
+              >
+                <FeedbackEmotion.Label label={label} />
+                {isToast && nowSelected === score && (
+                  <Toast style={{ width: 130 }}>
+                    <Text label="평가가 반영되었어요!" color={Theme.color.white} size="12px" bold="400" />
+                  </Toast>
+                )}
+              </FeedbackEmotion>
+            );
+          })}
+        </Select.List>
+      </Select>
     </StyledFeedbackSelect>
   );
 }
