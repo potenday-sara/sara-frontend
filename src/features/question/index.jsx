37fcdac75af8ab36dcf790fa9fad54c79adaf9cd
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2023-12-21 16:50:39 +0900

    Sar 39 유효성검사추가 (#106)
    * SAR-39 인풋 유효성검사 추가
    
    * SAR-39 쿠팡컴포넌트성능개선
    
    * lint수정
    
    * SAR-39-임시작업
    
    * SAR-39 사라애니메이션개발
    
    * SAR-39 구글폼버튼개발완료
    
    * SAR-39-질문유효성검사추가
    
    * SAR-39-피드백유효성검사추가
    
    * SAR-39 실패시 애니메이션추가
    
    * SAR-39 린트수정

--- src/features/question/index.jsx
+++ src/features/question/index.jsx
@@ -4,6 +4,7 @@ import QuestionTemplate from './components/templates/QuestionTemplate';
 import SaramaraServiceTemplate from './components/templates/SaramaraServiceTemplate';
 import Footer from '../../components/organisms/Footer';
 import QuestionNavbar from './components/organisms/QuestionNavbar';
+import NavigateGoogleForm from '../../components/organisms/NavigateGoogleForm';
 
 export default function QuestionPage() {
   return (
@@ -16,6 +17,7 @@ export default function QuestionPage() {
             <QuestionTemplate type="sara">
               <SaramaraServiceTemplate type="sara" />
             </QuestionTemplate>
+            <NavigateGoogleForm type="sara" />
             <Footer style={{ minWidth: 1400 }} />
           </>
         }
@@ -28,6 +30,7 @@ export default function QuestionPage() {
             <QuestionTemplate type="mara">
               <SaramaraServiceTemplate type="mara" />
             </QuestionTemplate>
+            <NavigateGoogleForm type="mara" />
             <Footer style={{ minWidth: 1400 }} />
           </>
         }
---
+++ src/hooks/useHover.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect, useRef } from 'react';
+
+// 사용자 정의 훅: useHover
+function useHover() {
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef(null);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener('mouseenter', handleMouseEnter);
+      node.addEventListener('mouseleave', handleMouseLeave);
+    }
+    // 정리(clean-up) 함수
+    return () => {
+      if (node) {
+        node.removeEventListener('mouseenter', handleMouseEnter);
+        node.removeEventListener('mouseleave', handleMouseLeave);
+      }
+    };
+  }, [ref.current]); // ref.current가 변경될 때마다 효과를 재실행합니다.
+
+  return [ref, isHovered];
+}
+
+export default useHover;
