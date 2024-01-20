ba46fdc4bf7e49eabe434f8d938a5701271b9c36
JaeHyun-Lim-FE <bonny06@naver.com>
2024-01-20 19:18:46 +0900

    :bug: fix: 질문페이지 캐러셀 텍스트 수정

--- src/features/question/components/organisms/QuestionSlider/index.jsx
+++ src/features/question/components/organisms/QuestionSlider/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import StyledQuestionSlider from './styles';
-import Text from '../../../../../components/atoms/Text';
-import Slider from '../../molecules/Slider';
-import useQuestions from '../../../hooks/useQuestions';
+import React from 'react';
 import { Theme } from '../../../../../Styles';
+import Text from '../../../../../components/atoms/Text';
 import useResize from '../../../../../hooks/useResize';
+import useQuestions from '../../../hooks/useQuestions';
 import HorizonSlider from '../../molecules/HorizonSlider';
+import Slider from '../../molecules/Slider';
+import StyledQuestionSlider from './styles';
 
 export default function QuestionSlider({ type }) {
   const { data: OtherQuestions, isLoading: QuestionLoading } = useQuestions(type);
   const { sizeType } = useResize();
   return (
     <StyledQuestionSlider>
-      <Text className="question-slider-title" label="다른 사람들의 고민" color={Theme.color.black} bold="700" />
+      <Text
+        className="question-slider-title"
+        label={type === 'sara' ? '다른 사람들은 어떠샤?' : '다른 사람들은 어떠먀?'}
+        color={Theme.color.black}
+        bold="700"
+      />
       {sizeType === 'medium' && !QuestionLoading && <Slider datas={OtherQuestions} type={type} />}
       {sizeType === 'small' && !QuestionLoading && <HorizonSlider datas={OtherQuestions} type={type} />}
     </StyledQuestionSlider>
