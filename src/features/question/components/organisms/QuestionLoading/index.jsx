a24cc7d61771180449907f2cad72cade2828fc66
JaeHyun-Lim-FE <bonny06@naver.com>
2024-01-25 15:28:34 +0900

    :lipstick: feat: 재시도 횟수별/로딩페이지 텍스트 중앙정렬 하도록 수정

--- src/features/question/components/organisms/QuestionLoading/index.jsx
+++ src/features/question/components/organisms/QuestionLoading/index.jsx
@@ -14,16 +14,16 @@ import StyledQuestionLoading from './styles';
 export default function QuestionLoading({ type, QuestionFormData, progress }) {
   const { onPreventLeave, offPreventLeave } = usePreventLeave();
   const saraRandomTextArray = [
-    '질문 내용을 자세하게 물어보면 더 좋은 답변을 받을 수 있을 거예요!',
-    '어떤 상품은 Sara가 잘 모를 수도 있어요...! 예를 들면... "비트겐슈타인 그란데 사이즈 3종 세트" 같은 거?',
-    'Sara의 답변이 마음에 드신다면, 하단의 평가 버튼을 눌러주세요! 더 좋은 조언을 주는 데에 힘이 될 거예요!',
-    '여기 어딘가에 정말 멋진 상품들이 있는 거 같아요! 클릭하면 대체 어떤 좋은 일이 일어날까요? (엇, 너무 속 보였나...?)',
+    '질문 내용을 자세하게 물어보면\n더 좋은 답변을 받을 수 있을 거예요!',
+    '어떤 상품은 Sara가 잘 모를 수도 있어요...!\n예를 들면... [비트겐슈타인 그란데 사이즈 3종 세트] 같은 거?',
+    'Sara의 답변이 마음에 드신다면,\n하단의 평가 버튼을 눌러주세요!\n더 좋은 조언을 주는 데에 힘이 될 거예요!',
+    '여기 어딘가에 정말 멋진 상품들이\n있는 거 같아요! 클릭하면 대체 어떤 좋은 일이 일어날까요?\n(엇, 너무 속 보였나...?)',
   ];
   const maraRandomTextArray = [
-    '질문 내용을 자세하게 물어보면 더 좋은 답변을 받을 수 있을 거예요!',
-    '어떤 상품은 Mara가 잘 모를 수도 있어요...! 예를 들면... "비트겐슈타인 그란데 사이즈 3종 세트" 같은 거?',
-    'Mara의 답변이 마음에 드신다면, 하단의 평가 버튼을 눌러주세요! 더 좋은 조언을 주는 데에 힘이 될 거예요!',
-    '여기 어딘가에 정말 멋진 상품들이 있는 거 같아요! (속닥속닥)근데 Mara는 그게 영 못 마땅한가 봐요...!',
+    '질문 내용을 자세하게 물어보면\n더 좋은 답변을 받을 수 있을 거예요!',
+    '어떤 상품은 Mara가 잘 모를 수도 있어요...!\n예를 들면... [비트겐슈타인 그란데 사이즈 3종 세트] 같은 거?',
+    'Mara의 답변이 마음에 드신다면,\n하단의 평가 버튼을 눌러주세요!\n더 좋은 조언을 주는 데에 힘이 될 거예요!',
+    '여기 어딘가에 정말 멋진 상품들이\n있는 거 같아요! (속닥속닥)근데 Mara는 그게 영 못 마땅한가 봐요...!',
   ];
   const [randomText, setRandomText] = useState('');
   useEffect(() => {
@@ -55,7 +55,7 @@ export default function QuestionLoading({ type, QuestionFormData, progress }) {
         ) : (
           <MaraCoupangRecommed width="90%" height="auto" />
         )}
-        <Text label={randomText} color={Theme.color.midGray} size="14px" bold="500" />
+        <Text label={randomText} color={Theme.color.midGray} style={{ textAlign: 'center' }} size="18px" bold="700" />
       </div>
       <QuestionInfo type={type} QuestionFormData={QuestionFormData} style={{ marginBottom: 24 }} />
     </StyledQuestionLoading>
