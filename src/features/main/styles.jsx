21b3c430d56228757910b6c8662446453c9b98fe
heogeon0 <heo3793@naver.com>
2024-02-04 22:23:40 +0900

    SAR-95 길이 CSS수정

--- src/features/main/styles.jsx
+++ src/features/main/styles.jsx
@@ -4,14 +4,15 @@ import { Theme } from '../../Styles';
 const StyledMain = styled.main`
   display: grid;
   grid-template-rows: 80px auto 64px;
-  grid-template-columns: minmax(512px, 4fr) minmax(220px, 1fr) minmax(512px, 4fr);
+  grid-template-columns: minmax(512px, 4fr) 240px minmax(512px, 4fr);
   min-height: 100vh;
   width: 100%;
-
+  ${Theme.deviceSize.medium} {
+    grid-template-columns: minmax(512px, 4fr) 208px minmax(512px, 4fr);
+  }
   ${Theme.deviceSize.small} {
     grid-template-columns: 1fr;
     grid-template-rows: minmax(252px, 50vh) minmax(252px, 50vh) auto;
-
     .main-aside {
       grid-row: 3 / 4;
     }
