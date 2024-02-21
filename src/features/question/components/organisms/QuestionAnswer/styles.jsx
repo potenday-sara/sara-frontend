e5dd44d9fd5d0b01aa8be9ea55025f0521215e0b
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2024-02-21 22:48:18 +0900

    Sar 98 카카오톡공유하기추가 (#134)
    * SAR-98 태그위치 수정 및 불필요이미지 제거
    
    * SAR-98결과 로직변경
    
    * SAR-98 배치 수정
    
    * SAR-98 일차 디자인 추가
    
    * SAR-98 인덱스 파일 수정

--- src/features/question/components/organisms/QuestionAnswer/styles.jsx
+++ src/features/question/components/organisms/QuestionAnswer/styles.jsx
@@ -4,6 +4,8 @@ import { Theme } from '../../../../../Styles';
 const StyledQuestionAnswer = styled.section`
   width: 100%;
   position: relative;
+  display: flex;
+  flex-direction: column;
   .question-top {
     width: 100%;
     display: flex;
@@ -16,6 +18,7 @@ const StyledQuestionAnswer = styled.section`
 
   .question-mid {
     width: 100%;
+    gap: 14px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -23,16 +26,20 @@ const StyledQuestionAnswer = styled.section`
   .button-wrap {
     width: 100%;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     gap: 8px;
   }
   .button {
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 79px;
     gap: 8px;
     .title {
+      flex-direction: column;
       justify-content: center;
+      align-items: center;
+      gap: 6px;
       p {
         line-height: 95%;
       }
@@ -40,14 +47,47 @@ const StyledQuestionAnswer = styled.section`
   }
   .answer-bottom {
     width: 100%;
-    margin: 24px 0px 0px;
+    margin: 0px 0px 0px;
+    padding: 10px;
+    color: #999;
+    font-size: 13px;
+    border: 1px solid ${({ type }) => (type === 'sara' ? Theme.color.saraSecondary : Theme.color.maraSecondary)};
+    border-radius: 14px;
+    background-color: ${({ type }) => (type === 'sara' ? '#fafcff' : '#fffbfa')};
+  }
+
+  .share {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    .buttons {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .share-button-wrap {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        gap: 4px;
+        .share-button {
+          width: 56px;
+          height: 56px;
+        }
+      }
+    }
   }
 
   ${Theme.deviceSize.large} {
     .button-wrap {
       .button {
         border-radius: 12px;
-        height: 52px;
+        /* height: 52px; */
       }
     }
   }
@@ -55,7 +95,7 @@ const StyledQuestionAnswer = styled.section`
     .button-wrap {
       .button {
         border-radius: 10px;
-        height: 48px;
+        /* height: 48px; */
       }
     }
   }
