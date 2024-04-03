5f6b87d58fd3b9c25251f1825799206a33a0ed14
heogeon0 <heo3793@naver.com>
2024-04-03 22:54:00 +0900

    feat: 좋아요 버튼 ui 구현

--- src/features/community/index.jsx
+++ src/features/community/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from './components/molecules/navbar';
 import Layout from './components/molecules/layout';
 import CommentList from './components/organisms/comments';
+
+import { ReactComponent as SaraCharacter } from './assets/saracharacter.svg';
+import { ReactComponent as SaraTitle } from './assets/saratitle.svg';
+import { ReactComponent as MaraTitle } from './assets/maratitle.svg';
+import Text from '../../components/atoms/Text';
+import { Theme } from '../../Styles';
 
 const COMMENTS_LIST = [
   {
@@ -24,13 +31,47 @@ const COMMENTS_LIST = [
   },
 ];
 
+const StyledQuestionInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  padding-bottom: 20px;
+  padding-top: 20px;
+  border-bottom: 1px solid #ddd;
+`;
+
+const StyledQuestionAnswer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 20px 0;
+  svg {
+    margin-bottom: 20px;
+  }
+  border-bottom: 1px solid #ddd;
+`;
+
 export default function Community() {
   return (
     <>
       <Navbar />
       <Layout>
-        <div>안녕하dd세요</div>
+        <StyledQuestionInformation>
+          <SaraTitle />
+          <div className="text">
+            <Text label="안녕하세요" size="20px" bold="700" />
+            <Text label="이미 잃어버려쎴는데 어쩌구 저쩌구" size="14px" style={{ marginTop: '3px' }} />
+          </div>
+        </StyledQuestionInformation>
+        <StyledQuestionAnswer>
+          <SaraCharacter />
+          <Text style={{ lineHeight: '22.4px' }} label="ㅋㄷㅋㄷ" size="14px" bold="500" color={Theme.color.darkGray} />
+        </StyledQuestionAnswer>
+
         <CommentList comments={COMMENTS_LIST} type="sara" />
+        <MaraTitle />
       </Layout>
     </>
   );
