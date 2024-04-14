43d393c93085d1f11f07e33c7e10885da6db597e
JaeHyun-Lim-FE <bonny06@naver.com>
2024-04-14 20:57:59 +0900

    :sparkle: feat: 질문리스트 정렬기준 선택버튼 삽입

--- src/features/lounge/components/organisms/LoungeHeader/index.jsx
+++ src/features/lounge/components/organisms/LoungeHeader/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ReactComponent as HeaderBackArrow } from '../../../assets/HeaderBackArrow.svg';
 import * as S from './styles';
 
 export default function LoungeHeader() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const handleNavigate = () => {
+    if (pathname?.includes('lounge')) navigate('/');
+    if (pathname?.includes('question')) navigate(-1);
+  };
   return (
     <S.StyledLoungeHeader>
       <S.StyledLoungeHeaderButton>
-        <HeaderBackArrow />
+        <HeaderBackArrow onClick={handleNavigate} />
       </S.StyledLoungeHeaderButton>
       <p>사라마라 라운지</p>
     </S.StyledLoungeHeader>
--- src/features/lounge/index.jsx
+++ src/features/lounge/index.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState } from 'react';
+import SortButtons from './components/molecules/SortButtons';
 import TypeButtons from './components/molecules/TypeButtons';
 import LoungeHeader from './components/organisms/LoungeHeader';
 import QuestionCards from './components/organisms/QuestionCards';
@@ -6,6 +7,7 @@ import LoungeContext from './stores/LoungeStore';
 
 export default function LoungePage() {
   const [type, setType] = useState('all');
+  const [sortType, setSortType] = useState('recent');
 
   const setAll = () => {
     setType('all');
@@ -16,14 +18,19 @@ export default function LoungePage() {
   const setMara = () => {
     setType('mara');
   };
+  const setSort = (s) => {
+    setSortType(s);
+  };
+
   const loungeStore = useMemo(() => {
-    return { type, setAll, setSara, setMara };
-  }, [type]);
+    return { type, sortType, setAll, setSara, setMara, setSort };
+  }, [type, sortType]);
 
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <LoungeContext.Provider value={loungeStore}>
         <LoungeHeader type={type} />
+        <SortButtons />
         <TypeButtons />
         <QuestionCards />
       </LoungeContext.Provider>
