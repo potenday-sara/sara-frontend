563deafe0219254da5dd080355369ab81ab33d20
heogeon0 <heo3793@naver.com>
2024-05-13 08:54:55 +0900

    refactor : tailwind color 추가

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        mara : {
+          'primary': '#EF2B00',
+          '50%': '#F79580',
+          '25%': '#FBCABF',
+          'secondary': '#FDEAE5',
+        },
+        sara :  {
+          'primary': '#007BED',
+          '50%': '#808DF6',
+          '25%': '#BFDEFB',
+          'secondary': '#E5F2FD',
+        },
+        black : {
+          '900' : '#333333',
+          '800' : '#666666',
+          '700' : '#999999',
+          '600' : '#CCCCCC',
+          '500' : '#dddddd',
+          '400' : '#EEEEEE',
+          '300' : '#F4F4F4',
+        },
+        error : '#FF4040',
+      },
+
+    },
+  },
+  plugins: [],
+}
