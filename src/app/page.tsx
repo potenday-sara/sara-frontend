00c667e2e1b82f773b2468e2aa923281b42c810b
heogeon0 <heo3793@naver.com>
2024-05-15 21:57:50 +0900

    feat : font 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,3 @@
 export default function page({}) {
-  return <div className={'bg-sara-50% w-[100px] h-[100px] text-sara-25%'}>페이지</div>;}
+  return <div className="bg-sara-50% w-[100px] h-[100px] text-sara-25 font-24-title-140">페이지</div>;
+}
