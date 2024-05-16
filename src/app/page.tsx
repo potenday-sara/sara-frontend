0b01600f1284da2972be558c3623677857c2a8d1
heogeon0 <heo3793@naver.com>
2024-05-16 23:26:48 +0900

    feat : 모바일 / 데스크탑 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function page({}) {
-  return <div className="bg-sara-50% w-[100px] h-[100px] text-sara-25 font-24-title-140">페이지</div>;
+import { redirect } from 'next/navigation';
+import Logo from '@/components/atoms/Logo/Logo';
+
+export default function page({ searchParams, params }) {
+  const { viewPort } = searchParams;
+  if (viewPort === 'mobile') {
+    redirect('/mobile');
+  }
+  redirect('/desktop');
+
+  return (
+    <div className="bg-sara-50% text-sara-25 font-24-title-140 text-mara-primary">
+      <Logo size="sm" type="sara" />
+      <p>페이지</p>
+    </div>
+  );
 }
