0e5aadb26a16254da3519947593d322d37a390f0
heogeon0 <heo3793@naver.com>
2024-06-22 18:35:18 +0900

    feat : server action 실행

--- src/libs/server-fetch.ts
+++ src/libs/server-fetch.ts
@@ -1,25 +1,25 @@
 import { HTTPMethod, HTTPParams, HTTPHeaders, HTTPServerOptions } from '@/libs/index';
 import API from '@/libs/fetch';
 
-export default class ServerAPI extends API{
-  options: HTTPServerOptions
+export default class ServerAPI extends API {
+  options: HTTPServerOptions;
 
   constructor(method: HTTPMethod, url: string) {
     super(method, url);
   }
 
   call<T>(): Promise<T> {
-    const URL = this.baseURL + this.url
+    const queryString = new URLSearchParams(this.params).toString();
+    const URL = this.baseURL + this.url + (queryString ? `?${queryString}` : '');
     return fetch(URL, {
       body: JSON.stringify(this.data),
-      ...this
-    }).then((res: Response) => {
-      if (res.ok) {
-        return res.json()
-      } else {
-        throw new Error(res.statusText)
-      }
+      ...this,
     })
+      .then((res: Response) => {
+        return res.json();
+      })
+      .catch((error) => {
+        return error.json();
+      });
   }
 }
-
