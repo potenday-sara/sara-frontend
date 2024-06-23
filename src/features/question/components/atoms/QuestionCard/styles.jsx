f4d791e6942342e464da58d4a52aeb1fa92beed2
JaeHyun-Lim-FE <bonny06@naver.com>
2024-06-23 23:35:31 +0900

    :art: fix: 모바일 캐러셀 arrow 추가

--- src/features/question/components/atoms/QuestionCard/styles.jsx
+++ src/features/question/components/atoms/QuestionCard/styles.jsx
@@ -18,6 +18,9 @@ const StyledCard = styled.div`
     height: 145px;
     padding: 28px;
     font-size: 16px;
+    .questioncard-arrow {
+      display: none;
+    }
     .card-title {
       font-size: 20px;
     }
@@ -31,6 +34,9 @@ const StyledCard = styled.div`
     width: 320px;
     height: 124px;
     padding: 20px;
+    .questioncard-arrow {
+      display: none;
+    }
     .card-title {
       font-size: 16px;
     }
@@ -39,16 +45,20 @@ const StyledCard = styled.div`
       line-height: 135%;
     }
   }
-  // 1200이상 디자인
+  // 1200이하 디자인
   ${Theme.deviceSize.small} {
     width: 204px;
     height: 56px;
     padding: 10px;
     gap: 5px;
-
+    .questioncard-arrow {
+      display: flex;
+    }
     .card-title {
       font-size: 14px;
       line-height: 100%;
+      display: flex;
+      align-items: center;
     }
     .card-content {
       font-size: 12px;
