e59e0cef2839784713291da19e73fed8b9ed7c81
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2024-08-13 23:40:20 +0900

    fix : 타입에러 해결 (#173)

--- src/app/i18n/setting.ts
+++ src/app/i18n/setting.ts
@@ -1,9 +1,9 @@
-import { i18n } from '@/app/i18n/i18n.config';
+import { i18n, Locale } from '@/app/i18n/i18n.config';
 
 export const languages = [...i18n.locales];
 export const defaultNS = 'main';
 
-export function getOptions(lng = i18n.defaultLocale, ns: string | string[] = defaultNS) {
+export function getOptions(lng: Locale = i18n.defaultLocale, ns: string | string[] = defaultNS) {
   return {
     supportedLngs: languages,
     fallbackLng: i18n.defaultLocale,
