e956fb9a1e18858a217e7ac2d53be0c76cc05b1e
heogeon0 <heo3793@naver.com>
2024-09-09 22:49:52 +0900

    feat : 라우팅 바꾸는 함수

--- src/app/[lang]/question/_components/LanguageSelect.tsx
+++ src/app/[lang]/question/_components/LanguageSelect.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { cva } from 'class-variance-authority';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { Locale } from '@/app/i18n/i18n.config';
 import BottomArrow from '@/app/_asset/bottom-arrow.svg';
 import Globe from '@/app/_asset/globe.svg';
@@ -19,6 +20,18 @@ interface Props {
 export default function LanguageSelect({ lang }: Props) {
   const [isLanguageSelectOpen, setIsLanguageSelectOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState<Locale>(lang);
+  const pathname = usePathname(); // 현재 경로를 가져옴
+  const searchParams = useSearchParams(); // 쿼리 파라미터를 가져옴
+  const router = useRouter(); // 라우터 가져오기
+
+  const handleLanguageSelect = (language: Locale) => {
+    if (!pathname || !searchParams) return;
+    const updatedPathname = pathname.replace(`/${lang}/`, `/${language}/`);
+
+    const updatedSearchParams = searchParams.toString();
+
+    router.push(`${updatedPathname}?${updatedSearchParams}`);
+  };
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
@@ -39,7 +52,7 @@ export default function LanguageSelect({ lang }: Props) {
     <>
       {isLanguageSelectOpen && (
         <div className="border font-14-title-100 text-neutral-500 shadow flex flex-col w-[78px] overflow-hidden rounded-[8px]">
-          <button type="button" className="hover:bg-neutral-50" onClick={() => setSelectedLanguage('ko')}>
+          <button type="button" className="hover:bg-neutral-50" onClick={() => handleLanguageSelect('ko')}>
             {selectedLanguage === 'ko' ? (
               <div className={buttonClassName({ isSelected: selectedLanguage === 'ko' })}>
                 <Globe />
@@ -52,7 +65,7 @@ export default function LanguageSelect({ lang }: Props) {
           <button
             className="border-solid border-t-[1px] border-b-[1px] border-neutral-100 hover:bg-neutral-50 "
             type="button"
-            onClick={() => setSelectedLanguage('en')}
+            onClick={() => handleLanguageSelect('en')}
           >
             {selectedLanguage === 'en' ? (
               <div className={buttonClassName({ isSelected: selectedLanguage === 'en' })}>
@@ -63,7 +76,7 @@ export default function LanguageSelect({ lang }: Props) {
               <div className="flex justify-center items-center h-9">EN</div>
             )}
           </button>
-          <button type="button" className="hover:bg-neutral-50" onClick={() => setSelectedLanguage('jp')}>
+          <button type="button" className="hover:bg-neutral-50" onClick={() => handleLanguageSelect('jp')}>
             {selectedLanguage === 'jp' ? (
               <div className={buttonClassName({ isSelected: selectedLanguage === 'jp' })}>
                 <Globe />
