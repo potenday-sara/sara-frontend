1a585cf047416d41fc3b410d842422cbe47f8ade
Heo Geon Nyoung <97589715+heogeon0@users.noreply.github.com>
2024-09-11 22:12:13 +0900

    feat : 메타데이터 추가 (#180)

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script';
 import { ReactNode } from 'react';
 import Footer from '@/components/organisms/Footer/Footer';
 import { i18n, type Locale } from '@/app/i18n/i18n.config';
+import { getDictionary } from '@/getDictionary';
 
 type SARA = 'sara';
 type MARA = 'mara';
@@ -17,22 +18,22 @@ export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
-export default function RootLayout({ children, params }: { children: ReactNode; params: { lang: Locale } }) {
+export default async function RootLayout({ children, params }: { children: ReactNode; params: { lang: Locale } }) {
+  const dictionary = await getDictionary();
   return (
     <html lang={params.lang}>
       <head>
         <meta charSet="UTF-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Sara Mara</title>
-        <meta name="description" content="Web site created..." />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-G3W9N0F90R" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3538415329975370"
           crossOrigin="anonymous"
         />
-        <title>살까 말까 할 땐, "Sara"</title>
+        <title>{dictionary.title[params.lang]}</title>
+        <meta name="descrition" content={dictionary.description[params.lang]} />
         <Script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js"
           integrity="sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01"
